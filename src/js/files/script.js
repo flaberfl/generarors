29e799fb7c56532593332ff4b9a71660c7b7e778
Андрей Олейник <48767673+flaberfl@users.noreply.github.com>
2023-12-09 13:51:53 +0300

    work

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -120,32 +120,32 @@ quizItems.forEach((quizItem, quizItemIndex) => {
 /*  Yandex Map   */
 
 
-initMap();
+// initMap();
 
-async function initMap() {
-  // Промис `ymaps3.ready` будет зарезолвлен, когда загрузятся все компоненты основного модуля API
-  await ymaps3.ready;
+// async function initMap() {
+//   // Промис `ymaps3.ready` будет зарезолвлен, когда загрузятся все компоненты основного модуля API
+//   await ymaps3.ready;
 
-  const { YMap, YMapDefaultSchemeLayer } = ymaps3;
+//   const { YMap, YMapDefaultSchemeLayer } = ymaps3;
 
-  // Иницилиазируем карту
-  const map = new YMap(
-    // Передаём ссылку на HTMLElement контейнера
-    document.getElementById('map'),
+//   // Иницилиазируем карту
+//   const map = new YMap(
+//     // Передаём ссылку на HTMLElement контейнера
+//     document.getElementById('map'),
 
-    // Передаём параметры инициализации карты
-    {
-      location: {
-        // Координаты центра карты
-        center: [37.588144, 55.733842],
+//     // Передаём параметры инициализации карты
+//     {
+//       location: {
+//         // Координаты центра карты
+//         center: [37.588144, 55.733842],
 
-        // Уровень масштабирования
-        zoom: 100
-      }
-    }
-  );
+//         // Уровень масштабирования
+//         zoom: 100
+//       }
+//     }
+//   );
 
-  // Добавляем слой для отображения схематической карты
-  map.addChild(new YMapDefaultSchemeLayer());
-}
+//   // Добавляем слой для отображения схематической карты
+//   map.addChild(new YMapDefaultSchemeLayer());
+// }
 
