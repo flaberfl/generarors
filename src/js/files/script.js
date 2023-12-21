7907ae7cc9ff84fa65aa40099c4fc3f61e70d699
Андрей Олейник <48767673+flaberfl@users.noreply.github.com>
2023-12-22 00:48:56 +0300

    home

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,72 +3,22 @@ import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
 
-// При нажатии на "Показать еще", появляются все card
 
-const btn = document.querySelector('.show-more');
-const cards = Array.from(document.querySelectorAll('.item-catalog-more'));
+const showMoreBtns = document.querySelectorAll('.show-more');
 
-window.addEventListener('resize', event => {
-  if (event.target.window.innerWidth > 992) response1();
-  if (event.target.window.innerWidth <= 992 && event.target.window.innerWidth > 680) response2();
-  if (event.target.window.innerWidth <= 680) response3();
-})
+showMoreBtns.forEach(btn => {
+  btn.addEventListener('click', event => {
+    const currentBtn = event.currentTarget;
+    const currentTab = currentBtn.closest('.catalog__tabs-body');
+    const catalogItems = currentTab.querySelector('.catalog__items');
+    const catalogItem = catalogItems.querySelectorAll('.catalog__item');
 
-function openCatalog() {
-  btn.addEventListener('click', () => {
-    cards.forEach(item => item.classList.remove('hidden-content'));
-    btn.classList.add('hidden-content');
+    for (let i = 2; i < catalogItem.length; i++) {
+      catalogItem[i].classList.toggle('hidden-content');
+    }
+    currentTab.classList.toggle('show');
   })
-}
-
-function response1() {
-  if (window.innerWidth > 992) {
-
-    cards.forEach((item, index) => {
-      item.classList.add('hidden-content')
-      if (index <= 2) {
-        item.classList.remove('hidden-content')
-      } else if (index > 2) {
-        btn.classList.remove('hidden-content');
-      }
-      openCatalog()
-    })
-  }
-}
-response1()
-
-function response2() {
-  if (window.innerWidth <= 992 && window.innerWidth > 680) {
-
-    cards.forEach((item, index) => {
-      item.classList.add('hidden-content')
-      if (index <= 3) {
-        item.classList.remove('hidden-content')
-      } else if (index > 3) {
-        btn.classList.remove('hidden-content');
-      }
-      openCatalog()
-    })
-  }
-}
-response2()
-
-function response3() {
-  if (window.innerWidth <= 680) {
-
-    cards.forEach((item, index) => {
-      item.classList.add('hidden-content')
-      if (index <= 2) {
-        item.classList.remove('hidden-content')
-      } else if (index > 2) {
-        btn.classList.remove('hidden-content');
-      }
-      openCatalog()
-    })
-  }
-}
-response3()
-
+})
 
 // Активный класс тобам
 
