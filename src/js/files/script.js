106673b70c9174255e40e068fa6d410c9ac0c564
Андрей Олейник <48767673+flaberfl@users.noreply.github.com>
2023-12-23 11:57:30 +0300

    Update script.js

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -198,7 +198,7 @@ dataValue.forEach(inp => {
 
 ymaps.ready(init);
 function init() {
-  var myMap = new ymaps.Map("map", {
+  var map = new ymaps.Map("map", {
     center: [55.825087, 37.588171],
     zoom: 18
   });
@@ -213,5 +213,20 @@ function init() {
       iconImageSize: [59, 78],
       iconImageOffset: [-30, -78]
     });
-    myMap.geoObjects.add(myPlacemark);
+
+
+  map.controls.remove('geolocationControl');
+  map.controls.remove('searchControl'); // удаляем поиск
+  map.controls.remove('trafficControl'); // удаляем контроль трафика
+  map.controls.remove('typeSelector'); // удаляем тип
+
+  // map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+  // map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+  map.controls.remove('rulerControl'); // удаляем контрол правил
+  map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+
+
+
+
+  map.geoObjects.add(myPlacemark);
 }
